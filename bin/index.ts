4dac8d45d6cefa6396a7637fc0cdfddb4c02752d
rkskekzzz <wkdlfflxh@naver.com>
2022-10-13 15:45:01 +0900

    :tada: project init

---
+++ bin/index.ts
@@ -0,0 +1,10 @@
+#! /usr/bin/env node
+import { appendFile, mkdir } from 'fs';
+
+mkdir('./dir/test', { recursive: true }, (err) => {
+  if (err) throw err;
+  appendFile('dir/test/test.txt', 'Hello world!', (err) => {
+    if (err) throw err;
+    console.log('Saved!');
+  });
+});
