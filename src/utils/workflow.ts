832ffaf641a17deec815ca74526059592cfa8bdd
rkskekzzz <wkdlfflxh@naver.com>
2022-10-14 20:48:18 +0900

    ♻️ workflowdir 변수로 분리

--- src/utils/workflow.ts
+++ src/utils/workflow.ts
@@ -3,15 +3,16 @@ import { stringify } from 'yaml';
 import { Answer } from '../questions.js';
 import { pushImagePreset } from '../preset/pushImage.js';
 
+const workflowDir = './.github/workflows';
+
 function createWorkflowContents(answer: Answer): string {
   return stringify(pushImagePreset(answer));
 }
 
 export function createWorkflowFile(answer: Answer) {
-  mkdir('./.github/workflow', { recursive: true }, (err) => {
+  mkdir(workflowDir, { recursive: true }, (err) => {
     if (err) throw err;
-    ``;
-    appendFile(`./.github/workflow/${answer.fileName}`, createWorkflowContents(answer), (err) => {
+    appendFile(`${workflowDir}/${answer.fileName}`, createWorkflowContents(answer), (err) => {
       if (err) throw err;
       console.log('Saved!');
     });
